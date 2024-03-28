0ac7f8e68159d77c953aa1146bbc95fdc548da42
mufid1012 <mufidghibran1012@gmail.com>
2024-03-28 15:20:32 +0700

    membuat step 5

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -51,7 +51,7 @@ void q_short ( int low, int high)
 
 
     {
-        while ((arr[i] <= pivot) && (i <=high)) 
+        while ((arr[i] <= pivot) && (i <=high)) //step 5
         {
             
         }
