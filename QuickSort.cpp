7b57250cd79a48e59ec87152f4d3a98c8716ed4d
mufid1012 <mufidghibran1012@gmail.com>
2024-03-28 15:25:06 +0700

    Membuat step 9

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -61,7 +61,11 @@ void q_short ( int low, int high)
         while ((arr[j]> pivot) && (j >= low)) //step 7
         {
             j--; //step 8
+            cmp_count++;
         }
+        cmp_count++;
+        if (i < j) //step 9
+        {}
     }
 
 
