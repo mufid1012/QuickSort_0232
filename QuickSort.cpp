2fb597c5cf96ad6f15f8af32ee14c5535f930e54
mufid1012 <mufidghibran1012@gmail.com>
2024-03-28 15:11:01 +0700

    memasukan variabel swap

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -12,7 +12,27 @@ void input () {
         cout << "Masukan panjang element array: ";
         cin >> n 
 
-
-
+        if (n <= 20)
+            break;
+        else 
+            cout << "\nMasksimum panjang array adalah 20" << endl;
     }
+
+    cout << "\n-------------------" << endl;
+    cout << "\nEnter Array Element" << endl;
+    cout << "\n-------------------" << endl;
+
+    for (int i = 0; i <n; i++)
+    {
+        cout << "<" << (i + 1) << ">";
+        cin >> arr[i];
+    }
+}
+
+void swap (int x, int y)
+{
+    int temp = arr[x]
+    arr [x] = arr[y]
+    arr [y] = temp;
+    mov_count++;
 }
